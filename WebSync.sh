2eec73fdb8bff9bafd430e0909d8bfd0245effe9
Joseph Larson <jpl@showpage.org>
2023-10-11 10:58:27 -0500

    Added empty script.

---
+++ WebSync.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+# Run in a cron job to keep the a web site sync'd from a GitHub repo.
+#
+#   WebSync.sh srcdir destdir
+#   WebSync.sh ~jpl/LLPOA /Library/WebServer/Sites/llpoa.org/htdocs
+#
